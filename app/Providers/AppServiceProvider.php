e92a199d040cebecc9a58eb4d9449cee64b9aa6b
Sunera Sachintha <sachisune@gmail.com>
2022-02-14 20:04:47 +0530

    ssl issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ use App\Services\Contracts\BlogServiceInterface;
 use App\Services\Contracts\CommonServiceInterface;
 use App\Services\Contracts\ExternalApiCallsServiceInterface;
 use App\Services\Contracts\StorageServiceInterface;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -49,5 +50,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(env('APP_ENV') != "local"){
+            URL::forceScheme('https');
+        }
     }
 }
